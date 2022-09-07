94f12d0249b4b8397ca78b6c317f34e1a902d934
Greta Hibbard <gh945020@ohio.edu>
2022-09-07 17:02:35 -0400

    updated

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Greta Hibbard
+ *        Date: 9/7/22
+ * Description: lab1
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+   
+    return 0;
+}// main
